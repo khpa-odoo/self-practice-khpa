1fa6c912cb0347c6d0f735fc8e1de857746ab2f1
Khushi Patel <khpa@odoo.com>
2023-02-27 19:04:27 +0530

    Commit Feb 27

--- voguish/models/voguish_customers.py
+++ voguish/models/voguish_customers.py
@@ -10,4 +10,53 @@ class VoguishCustomers(models.Model):
     phone = fields.Char(related='partner_id.phone', store=True, readonly=False)
     mobile = fields.Char(related='partner_id.mobile', store=True, readonly=False)
     website = fields.Char(related='partner_id.website', readonly=False)
+    street = fields.Char(compute='_compute_address', inverse='_inverse_street')
+    street2 = fields.Char(compute='_compute_address', inverse='_inverse_street2')
+    zip = fields.Char(compute='_compute_address', inverse='_inverse_zip')
+    city = fields.Char(compute='_compute_address', inverse='_inverse_city')
+    state_id = fields.Many2one(
+        'res.country.state', compute='_compute_address', inverse='_inverse_state',
+        string="State", domain="[('country_id', '=?', country_id)]"
+    )
+    country_id = fields.Many2one('res.country', compute='_compute_address', inverse='_inverse_country', string="Country")
     
+    
+    def _get_company_address_field_names(self):
+        """ Return a list of fields coming from the address partner to match
+        on company address fields. Fields are labeled same on both models. """
+        return ['street', 'street2', 'city', 'zip', 'state_id', 'country_id']
+
+    def _get_company_address_update(self, partner):
+        return dict((fname, partner[fname])
+                    for fname in self._get_company_address_field_names())
+    
+    def _compute_address(self):
+        for company in self.filtered(lambda company: company.partner_id):
+            address_data = company.partner_id.sudo().address_get(adr_pref=['contact'])
+            if address_data['contact']:
+                partner = company.partner_id.browse(address_data['contact']).sudo()
+                company.update(company._get_company_address_update(partner))
+
+    def _inverse_street(self):
+        for company in self:
+            company.partner_id.street = company.street
+
+    def _inverse_street2(self):
+        for company in self:
+            company.partner_id.street2 = company.street2
+
+    def _inverse_zip(self):
+        for company in self:
+            company.partner_id.zip = company.zip
+
+    def _inverse_city(self):
+        for company in self:
+            company.partner_id.city = company.city
+
+    def _inverse_state(self):
+        for company in self:
+            company.partner_id.state_id = company.state_id
+
+    def _inverse_country(self):
+        for company in self:
+            company.partner_id.country_id = company.country_id
